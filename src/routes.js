df33266937efccf9d81f176d3add86c4a9743aac
Emerson Silva <emerson_jdss@hotmail.com>
2019-12-01 02:46:42 -0300

    marker loft

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
 import Login from './pages/Login';
+import Calculator from './pages/Calculator';
+import Maps from './pages/Maps';
 
 const Routes = createAppContainer(
   createStackNavigator(
     {
+      Calculator,
       Login,
+      Maps,
     },
     {
       headerMode: 'none',
