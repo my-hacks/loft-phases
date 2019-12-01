4985d645c2a013e58d8a93c2a7de3123c47cb0e7
Emerson Silva <emerson_jdss@hotmail.com>
2019-12-01 13:12:58 -0300

    add docus screen

--- src/pages/ImALofter/index.js
+++ src/pages/ImALofter/index.js
@@ -1,12 +1,70 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {
+  DefaultContainer,
+  DefaultSafeArea,
+  Container,
+  TextTitle,
+  ButtonSend,
+} from './styles';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+EvilIcons.loadFont();
 
 export class ImALofter extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <DefaultSafeArea>
+        <Container>
+          <DefaultContainer>
+            <TextTitle>
+              Parabens voce esta dando um passo muito importante na compra de um
+              imovel e iremos te acompanhar nesta etapa
+            </TextTitle>
+          </DefaultContainer>
+          <DefaultContainer>
+            <TextTitle>
+              Precisamos de alguns documentos para adiantar a papelada, voce
+              manda a foto nos cuidamos da burocracia.
+            </TextTitle>
+          </DefaultContainer>
+          <DefaultContainer
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <EvilIcons name="camera" size={45} color="orange" />
+            <Text>Comprovante de Renda</Text>
+          </DefaultContainer>
+          <DefaultContainer
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <EvilIcons name="camera" size={45} color="orange" />
+            <Text>Comprovante de Endereco</Text>
+          </DefaultContainer>
+
+          <DefaultContainer
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <EvilIcons name="camera" size={45} color="orange" />
+            <Text>RG / CPF </Text>
+          </DefaultContainer>
+
+          <ButtonSend>
+            <Text
+              style={{
+                color: 'orange',
+                fontSize: 18,
+                fontWeight: 'bold',
+              }}>
+              Enviar Documentos
+            </Text>
+          </ButtonSend>
+        </Container>
+      </DefaultSafeArea>
     );
   }
 }
--- src/pages/ImALofter/styles.js
+++ src/pages/ImALofter/styles.js
@@ -10,3 +10,24 @@ export const Container = styled.View`
   padding: ${metrics.basePadding}px;
   flex: 1;
 `;
+
+export const DefaultContainer = styled.View`
+  margin-top: 30px;
+  height: auto;
+  width: 100%;
+`;
+
+export const TextTitle = styled.Text`
+  color: black;
+  font-size: 20px;
+`;
+
+export const ButtonSend = styled.TouchableOpacity`
+  height: 52;
+  width: 100%;
+  background-color: #000;
+  border-radius: 15px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+`;
